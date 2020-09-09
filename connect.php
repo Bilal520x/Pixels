8da6fba830d21fa4e5de32c4860c786b1abda8ab
unknown <bilalghazol@gmail.com>
2020-09-09 22:32:16 +0300

    Mahran

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=pixels";
+$user = "root";
+$passwd = "";
+try{
+$conn=new PDO($dsn,$user,$passwd);
+
+  
+  $fuckyou = "yousef";
+}
+catch(Exception $e) {
+echo "System Error";
+} 
+$headers = apache_request_headers();
+foreach ($headers as $header => $value):
+endforeach;
+?>
